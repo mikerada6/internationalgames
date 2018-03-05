8687fed355ffb04017828aba542a457cd6a1dfcf
Michael Radaszkiewicz <mrada6@gmail.com>
2018-03-04 21:02:12 -0500

    Initial Commit

---
+++ app/src/main/java/com/tca/rez/tcainternationalgames/MainActivity.java
@@ -0,0 +1,19 @@
+package com.tca.rez.tcainternationalgames;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user touches the button */
+    public void submitButton(View view) {
+        // Do something in response to button click
+    }
+}
