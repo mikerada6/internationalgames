2f9e00ac25b2e50c06d3f5b120dcdd35fb9448dd
Michael Radaszkiewicz <mrada6@gmail.com>
2018-03-06 18:03:11 -0500

    Log in screen now gets the users first and last name.

---
+++ app/src/main/java/com/tca/rez/tcainternationalgames/GameLandingPage.java
@@ -0,0 +1,13 @@
+package com.tca.rez.tcainternationalgames;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameLandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_landing_page);
+    }
+}
--- app/src/main/java/com/tca/rez/tcainternationalgames/MainActivity.java
+++ app/src/main/java/com/tca/rez/tcainternationalgames/MainActivity.java
@@ -114,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        hideDialog();
 
     }
 
